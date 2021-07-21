8b40588572a5bfb77519e11134cbd2efaf1713f5
Romeo Folie <romeofolie1@gmail.com>
2021-07-21 11:57:07 +0000

    testing out fix for styled components ssr issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import Document, {
-  Html, Head, Main, NextScript,
-} from 'next/document';
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import React from "react";
+import Document, {Html, Head, Main, NextScript} from "next/document";
+import {ServerStyleSheets} from "@material-ui/core/styles";
+import {ServerStyleSheet} from "styled-components";
 
 export default class MyDocument extends Document {
   render() {
@@ -22,20 +21,43 @@ export default class MyDocument extends Document {
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with server-side generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
-
   // Render app and page and get the context of the page with collected side effects.
   const sheets = new ServerStyleSheets();
+  // Styled components
+  const sheet = new ServerStyleSheet();
   const originalRenderPage = ctx.renderPage;
 
-  ctx.renderPage = () => originalRenderPage({
-    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-  });
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
 
-  const initialProps = await Document.getInitialProps(ctx);
+    const initialProps = await Document.getInitialProps(ctx);
 
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+          {sheets.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+
+  // ctx.renderPage = () => originalRenderPage({
+  //   enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+  // });
+
+  // const initialProps = await Document.getInitialProps(ctx);
+
+  // return {
+  //   ...initialProps,
+  //   // Styles fragment is rendered after the app and page rendering finish.
+  //   styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  // };
 };
