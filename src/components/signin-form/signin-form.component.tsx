5833b6d52ae8b9d6ff1e4de0c848a723d04af00c
Romeo Folie <romeofolie1@gmail.com>
2021-07-27 22:12:13 +0000

    wired up signin form

--- src/components/signin-form/signin-form.component.tsx
+++ src/components/signin-form/signin-form.component.tsx
@@ -3,17 +3,69 @@ import {Container, Form, Title, Row, Button} from "./signin-form.styles";
 import Input from "../input/input.component";
 import Check from "../check/check.component";
 import {useForm, Controller, SubmitHandler} from "react-hook-form";
+import {auth} from "../../services/firebase/firebase";
 
-const Signin = () => {
+interface FormValues {
+  email: string;
+  password: string;
+}
+
+const initialValues: FormValues = {
+  email: "",
+  password: "",
+};
+
+const Signin: React.FC = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: {errors},
+  } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
+    // console.log(data);
+    const {email, password} = data;
+    const {user} = await auth.signInWithEmailAndPassword(email, password);
+    console.log("logged in user ", user);
+    // dispatch some kind of action to create the user
+  };
+
   return (
     <Container>
-      <Form autoComplete="off">
+      <Form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
         <Title>Welcome back</Title>
-        <Input label="Email address" name="email" />
-        <Input label="Password" name="password" />
+        <Controller
+          name="email"
+          control={control}
+          defaultValue={initialValues.email}
+          rules={{required: "This field is required"}}
+          render={({field}) => (
+            <Input
+              label="Email address"
+              {...field}
+              error={errors?.email?.message}
+            />
+          )}
+        />
+
+        <Controller
+          name="password"
+          control={control}
+          defaultValue={initialValues.password}
+          rules={{required: "This field is required"}}
+          render={({field}) => (
+            <Input
+              label="Password"
+              {...field}
+              error={errors?.password?.message}
+            />
+          )}
+        />
+
         <Row>
           <Check name="remember" label="Remember me" />
         </Row>
+
         <Button type="submit">Login</Button>
       </Form>
     </Container>
