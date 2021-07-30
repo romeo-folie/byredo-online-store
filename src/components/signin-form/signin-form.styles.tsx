81a60afb83f8375b6b7e24928ba68fe2627911d0
Romeo Folie <romeofolie1@gmail.com>
2021-07-30 07:39:19 +0000

    attempt #2 at fixing keyboard shift issue

--- src/components/signin-form/signin-form.styles.tsx
+++ src/components/signin-form/signin-form.styles.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import {Button as FButton} from "../form/form.styles";
 
-export const Container = styled.div`
+export const Container = styled.section`
   width: 50%;
   display: flex;
   flex-direction: column;
@@ -11,7 +11,7 @@ export const Container = styled.div`
     width: 100%;
     min-height: 100vh;
     justify-content: center;
-    padding: 50px 0 20px;
+    /* padding: 50px 0 20px; */
   }
 `;
 
@@ -27,7 +27,7 @@ export const Title = styled.div`
   font-size: 2rem;
   margin-bottom: 20px;
 
-  @media only screen and (max-width: 1000px){
+  @media only screen and (max-width: 1000px) {
     font-size: 1.8rem;
   }
 `;
