e2d0958c86d685177448425e1335fa72a1c80e9e
Romeo Folie <romeofolie1@gmail.com>
2021-09-06 16:49:18 +0000

    fixed input autofill color issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -60,6 +60,21 @@ ul {
 * {
   box-sizing: border-box;
 }
+
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+textarea:-webkit-autofill,
+textarea:-webkit-autofill:hover,
+textarea:-webkit-autofill:focus,
+select:-webkit-autofill,
+select:-webkit-autofill:hover,
+select:-webkit-autofill:focus {
+    /* border: 1px solid #ffffff; */
+    -webkit-text-fill-color: #000000;
+    -webkit-box-shadow: 0 0 0px 1000px #e3e3e3 inset;
+    transition: background-color 5000s ease-in-out 0s;
+}
 `;
 
 const theme = {
